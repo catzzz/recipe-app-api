35681c085c8c108224dbf30c3def0d1e9d64944b
BioInspira <jimmyleu@bioinspira.com>
2020-05-03 09:50:37 +0800

    Added create recipe feature.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -41,7 +41,7 @@ class RecipeSerializer(serializers.ModelSerializer):
         read_only_fields = ('id',)
 
 
-class RecipeDetailSerializer(RecipeSerializer
+class RecipeDetailSerializer(RecipeSerializer):
     """Serialize a recipe detail"""
     ingredients = IngredientSerializer(many=True, read_only=True)
     tags = TagSerializer(many=True, read_only=True)
